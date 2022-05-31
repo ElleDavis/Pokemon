12eee247a8e5ac4630f9d3d4c83829a368a78816
Elle Davis <ShanitriceDavis@gmail.com>
2022-05-31 13:50:52 -0400

    index.ejs view rendered at pokemon route and I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -9,11 +9,20 @@ const pokemon=require ("./Models/pokemon")
 console.log("Hey Ms.Parker")
 
 app.get ("/",(req,res)=>{
-    res.send('Welcome to the Pokemon App!');
+    res.send('Welcome to the Pokemon App!',  
+   // { data: pokemonData, }
+         );
 })
 
 app.get ("/pokemon",(req,res)=>{
-    res.render("Index.ejs")
+    res.render("Index.ejs",
+ { data: pokemon, }
+    )
+})
+
+app.get ("/pokemon/:id",(req,res)=>{
+    res.send(req.params.id);
+    console.log("whatever")
 })
 
 
